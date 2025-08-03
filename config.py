84b58598f11f35b16e2fe1fa83fa11fe35b264e4
BADNAM XD <yadavxd746@gmail.com>
2025-08-03 23:22:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -84,10 +84,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/otkoyb.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/cudvlq.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/fa1xas.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/cudvlq.jpg"
 )
 PLAYLIST_IMG_URL = "https://files.catbox.moe/pfjgmf.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/st6utj.jpg"
@@ -120,5 +120,6 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
