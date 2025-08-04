0179f14173c23eec89ae2b13bf11564b0d5b7028
BADNAM XD <yadavxd746@gmail.com>
2025-08-04 10:49:57 +0530

    Update play.py

--- VenomMusic/utils/inline/play.py
+++ VenomMusic/utils/inline/play.py
@@ -65,10 +65,10 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/lll_BADNAM_BABY_lll",
+                text="✰ ᴏᴡɴᴇʀ ✰", url="https://t.me/lll_BADNAM_BABY_lll",
             ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/+G-kGeNJKv_YxNTg9",
+                text="✰ sᴜᴩᴩᴏʀᴛ ✰", url="https://t.me/+G-kGeNJKv_YxNTg9",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -87,10 +87,10 @@ def stream_markup(_, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/ll_istkhar_baby_lll",
+                text="✰ ᴏᴡɴᴇʀ ✰", url="https://t.me/lll_BADNAM_BABY_lll",
             ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/+rHfbX06JYoNlNDg9",
+                text="✰ sᴜᴩᴩᴏʀᴛ ✰", url="https://t.me/+G-kGeNJKv_YxNTg9",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -168,4 +168,5 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
     ]
     return buttons
     
+
 
