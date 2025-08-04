638d5bdf00e8b670d1b7894b89d3594fa80470aa
BADNAM XD <yadavxd746@gmail.com>
2025-08-04 06:45:12 +0530

    Update play.py

--- VenomMusic/utils/inline/play.py
+++ VenomMusic/utils/inline/play.py
@@ -33,25 +33,22 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "◉——————————"
     elif 10 < umm < 20:
-        bar = "—◉————————"
-    elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "—◉—————————"
+    elif 20 < umm < 30:
+        bar = "——◉————————"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "———◉———————"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "————◉——————"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
-    elif 60 <= umm < 70:
-        bar = "——————◉———"
-    elif 70 <= umm < 80:
-        bar = "———————◉——"
-    elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "——————◉————"
+    elif 50 <= umm < 70:
+        bar = "———————◉———"
     else:
-        bar = "—————————◉"
+        bar = "——————————◉"
+
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
@@ -66,7 +63,15 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+      [
+            InlineKeyboardButton(
+                text="ᴏᴡɴᴇʀ", url="https://t.me/ll_ISTKHAR_BABY_lll",
+            ),
+            InlineKeyboardButton(
+                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/+rHfbX06JYoNlNDg9",
+            )
+        ],
+         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -79,6 +84,14 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+         ],
+        [
+            InlineKeyboardButton(
+                text="ᴏᴡɴᴇʀ", url="https://t.me/ll_istkhar_baby_lll",
+            ),
+            InlineKeyboardButton(
+                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/+rHfbX06JYoNlNDg9",
+            )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -90,11 +103,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"VenomPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"AyushPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"VenomPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"AyushPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
@@ -154,3 +167,4 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
         ],
     ]
     return buttons
+    
